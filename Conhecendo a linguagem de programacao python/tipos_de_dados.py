73ab813ee64712ab6aa627f35409dbac20313fa4
yuricapella <yuricapelladossantos@gmail.com>
2024-08-23 15:50:06 -0300

    changes

---
+++ Conhecendo a linguagem de programacao python/tipos_de_dados.py
@@ -0,0 +1,13 @@
+print(1+10+1000)
+print(1.5+1+0.5)
+print(True)
+print(False)
+print("python")
+
+# int()
+# float()
+# str()
+# bool()
+'''
+Se segurar Control e clicar nessas funções acima int,float,str,bool o visual studio te levará até o lugar onde estão armazenadas.
+'''
