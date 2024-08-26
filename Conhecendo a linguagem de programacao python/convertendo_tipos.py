44d7e297f71143a5e513e6c9f6d12782a3c150e1
yuricapella <yuricapelladossantos@gmail.com>
2024-08-26 12:06:43 -0300

    Foram adicionadas novas anotações em informacoes.txt e o arquivo convertendo_tipos.py

---
+++ Conhecendo a linguagem de programacao python/convertendo_tipos.py
@@ -0,0 +1,16 @@
+print(int(5.516518917))
+print(int("10"))
+
+print(float("10.10"))
+print(float(100))
+
+valor = 20
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print (100 / 2) #Divisão retorna FLOAT
+print (100 // 2) #Divisão que retorna o resultado INT mais próximo
+
+valor_dois = 10.0
+print(type(valor_dois))
