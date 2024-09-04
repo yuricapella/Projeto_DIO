2468fb9816bcb90968c756cdecf8ef00782268f1
yuricapella <yuricapelladossantos@gmail.com>
2024-09-03 23:06:57 -0300

    Pasta adicionada: operadores_aritmeticos com arquivos informacoes.txt e operadores_aritmeticos.py'

---
+++ operadores_aritimeticos/operadores_aritimeticos.py
@@ -0,0 +1,18 @@
+produto_1 = 15
+produto_2 = 10
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 + produto_2 * 2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 % produto_2)
+print(produto_1 ** produto_2)
+
+x = (5+9) * 3
+y = (20/4)-(15*3)+(10**2)
+z = (20/4)-15*((3+10)**2)
+
+print(f"x = {x}")
+print(f"y = {y}")
+print(f"z = {z}")
