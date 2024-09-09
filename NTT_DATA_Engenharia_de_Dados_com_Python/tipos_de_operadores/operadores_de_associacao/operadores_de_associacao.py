4c8176756fe4ba112729e98ad688f1c7ce29c6ea
yuricapella <yuricapelladossantos@gmail.com>
2024-09-09 13:09:59 -0300

    Adicionado: operadores_de_associacao

---
+++ NTT_DATA_Engenharia_de_Dados_com_Python/tipos_de_operadores/operadores_de_associacao/operadores_de_associacao.py
@@ -0,0 +1,14 @@
+frutas = ["laranja", "uva", "limão"]
+print("laranja" in frutas)
+print("laranja" not in frutas)
+print("-------------")
+print("abacaxi" in frutas)
+print("abacaxi" not in frutas)
+print("-------------")
+nome = "Yuri Capella"
+print("Lucas" in nome)
+print("Capella" in nome)
+print("Lucas" not in nome)
+print("-------------")
+x = (22 - 10) * 3
+print(x)
